9247b3cfcf6f413c3a45f54600167f300a219fa0
Baka <bvbouba@gmail.com>
2019-02-18 16:23:37 -0600

    5th commit

--- spiderweb.py
+++ spiderweb.py
@@ -1,8 +1,21 @@
-import time
+from lib.getHtml import getTitle
+from lib.getHtml import getLinks
+import random
+import datetime
 
+url = "https://en.wikipedia.org/wiki/Kevin_Bacon"
 
-name=input("what is your name?")
-print("hello "+name)
+title = getTitle(url)
 
-currenttime = time.time()
-print("The current time is:" +str(currenttime))
+if title is None:
+    print("URL not found")
+else:
+    print(title)
+
+random.seed(datetime.datetime.now())
+article ="/wiki/Kevin_Bacon"
+links = getLinks(article)
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
