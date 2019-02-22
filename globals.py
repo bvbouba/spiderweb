24632b60f59a5ded60594e801fbaefc52f3b266d
Baka <bvbouba@gmail.com>
2019-02-21 20:35:42 -0600

    6th commit

---
+++ globals.py
@@ -0,0 +1,10 @@
+def initialize():
+    global unit
+    global unit_text
+    global rounding
+    global market_indice
+
+    unit = 1000000
+    unit_text = "in millions"
+    rounding = 2
+    market_indice = "BRVM-10"
--- spiderweb.py
+++ spiderweb.py
@@ -1,21 +1,81 @@
-from lib.getHtml import getTitle
-from lib.getHtml import getLinks
-import random
-import datetime
+import globals
+from lib.getStockInfo import getStockPrice,getLastDay,getMarketCap,getChartData,getCompsData
+from lib.getFinStat import checkSymbol,updateFinData,getKPI
 
-url = "https://en.wikipedia.org/wiki/Kevin_Bacon"
 
-title = getTitle(url)
+#updateFinData()
 
-if title is None:
-    print("URL not found")
-else:
-    print(title)
+globals.initialize()
 
-random.seed(datetime.datetime.now())
-article ="/wiki/Kevin_Bacon"
-links = getLinks(article)
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
-    print(newArticle)
-    links = getLinks(newArticle)
+q=0
+while q==0:
+    #ticker = input("Quote search:")
+    ticker = "PALC"
+    sector = checkSymbol(ticker)
+    if sector == None:
+        print("Quote is not valid")
+    else:
+        q=1
+
+
+getChartData(ticker)
+
+quit()
+stock = getStockPrice(ticker,sector)
+stockTicker = stock[0]
+stockPrice = stock[1]
+priceChange = stock[2]
+
+print("Ticker:"+stockTicker)
+print("Current Price:"+stockPrice)
+print("Change in Price:"+priceChange)
+
+lastTrend = getLastDay(ticker)
+
+stockTicker = lastTrend[0]
+companyName = lastTrend[1]
+stockVolume = lastTrend[2]
+openPrice = lastTrend[3]
+prevClose = lastTrend[4]
+
+print("Company Name:"+companyName)
+print("Volume:"+stockVolume)
+print("Open Price:"+openPrice)
+print("Prev Close:"+prevClose)
+
+capData= getMarketCap(ticker)
+
+marketCap = capData[0]
+MarketCapChange = capData[1]
+shares = capData[2]
+
+print("Market Cap:"+marketCap)
+print("Change in MarkCap:"+MarketCapChange)
+
+#getChartData(ticker)
+kpi = getKPI(ticker,float(stockPrice),float(shares))
+
+eps = kpi[0]
+peRatio = kpi[1]
+ptobookRatio = kpi[2]
+ptoSales = kpi[3]
+fiscYear = kpi[4]
+print("-----------Key Stat----")
+print("EPS:"+eps)
+print("P/E Ratio:"+peRatio)
+print("Shares Outstanding:"+shares)
+print("Price to Book Ratio:"+ptobookRatio)
+print("Price to Sales Ratio:"+ptoSales)
+print("Earnings Announcement for fiscal period ending in "+fiscYear)
+
+print("-----------COMPARE 1 DAY performance----")
+
+compsData = getCompsData(sector,globals.market_indice)
+sector_change = compsData[0]
+market_change = compsData[1]
+
+print("EQUITY\n"+ticker+" : "+priceChange)
+print("SECTOR\n"+sector+" : "+sector_change)
+print("MARKET\n"+globals.market_indice+" : "+market_change)
+
+print("-----------ABOUT THE COMPANY----")
